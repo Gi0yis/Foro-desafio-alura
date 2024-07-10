20fa2ee6d1f0994cfcf6d3584eced94da3253dab
Gi0yis <gioyis_github@outlook.com>
2024-07-09 21:09:10 -0600

    Corrigiendo Errores

--- java-projecst/challenge-literalura/src/main/java/com/gioyisapplication/challenge_literalura/dto/BookDTO.java
+++
@@ -1,9 +0,0 @@
-package com.gioyisapplication.challenge_literalura.dto;
-
-public record BookDTO(
-         String title,
-         String author,
-         String languages,
-         Integer downloadCount
-) {
-}
